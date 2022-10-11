10380a7446fb0b44fcd2e5683270c4d7b2100e22
Nagi-ta <uextyou@gmail.com>
2022-10-11 08:05:11 +0000

    [Update]ルートディテクトリをホーム画面、resourcesメソッドでルーティングを自動生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
-  get 'index' => 'books#index'
-  get 'show' => 'books#show'
-  get 'edit' => 'books#edit'
+  root :to => 'homes#top'
+  resources :books
+  # get 'index' => 'books#index'
+  # get 'show' => 'books#show'
+  # get 'edit' => 'books#edit'
 end
