6bcdeadebcd0b66cd7f2b2443c8ef8d4400676a5
Nagi-ta <uextyou@gmail.com>
2022-10-12 07:18:45 +0000

    [Add] editページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,9 +16,20 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+    # book = Book.find(params[:id])
+    # book.update(book_params)
+    # redirect_to "/books/#{book.id}/show"
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path{book.id}
   end
 
   def destroy
