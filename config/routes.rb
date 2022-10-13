b0cefa3c564ac36ae1a9dd58e1dcf95517ecaa9c
Nagi-ta <uextyou@gmail.com>
2022-10-13 05:43:46 +0000

    [Fix] 不要なアクションを削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root :to => 'homes#top'
   resources :books
-  # get 'index' => 'books#index'
-  # get 'show' => 'books#show'
-  # get 'edit' => 'books#edit'
 end
